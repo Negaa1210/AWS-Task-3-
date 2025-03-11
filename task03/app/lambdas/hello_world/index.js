a2a7f9adb5a42c0bac8266290833137ce6699bc5
Neha Gupta4 <neha_gupta4@epam.com>
2025-03-11 12:04:40 +0530

    latest update

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -1,7 +1,8 @@
 exports.handler = async (event) => {
+    // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello, World!'),
+        message:"Hello from lambda"
     };
     return response;
 };
